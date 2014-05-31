5517842657b3cfb247c427778aad825dd96bca45
Jasper Van Damme <jasper.van.damme@student.howest.be>
2014-05-31 21:31:19 +0200

    added noscript info

--- _js/router/AppRouter.js
+++ _js/router/AppRouter.js
@@ -9,24 +9,24 @@ var AppRouter = Backbone.Router.extend({
     },
 
     routes: {
-        "": "overview",
-        "home/": "overview",
-        "tickets/:day": "dayView",
-        "dag/:day": "dayView"
+        '': 'overview',
+        'home/': 'overview',
+        'tickets/:day': 'dayView',
+        'dag/:day': 'dayView'
     },
 
     overview: function () {
-        console.log("[AppRouter] overview()");
+        console.log('[AppRouter] overview()');
         this.enRouteApp = new EnRouteApp();
-        $("#container").remove();
-        $("body").prepend(this.enRouteApp.render().$el);
+        $('#container, noscript').remove();
+        $('body').prepend(this.enRouteApp.render().$el);
     },
 
     dayView: function (day) {
-        console.log("[AppRouter] dayView()");
+        console.log('[AppRouter] dayView()');
         this.enRouteApp = new EnRouteApp();
         this.enRouteApp.currentScreen = day;
-        $("#container").remove();
-        $("body").prepend(this.enRouteApp.render().$el);
+        $('#container, noscript').remove();
+        $('body').prepend(this.enRouteApp.render().$el);
     }
 });
