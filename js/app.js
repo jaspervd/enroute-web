f7b9efa3ce76381569f83e0162df619580fcf32b
Jasper Van Damme <jasper.van.damme@student.howest.be>
2014-06-09 17:15:05 +0200

    prevented audio playing every mousemove

--- js/app.js
+++ js/app.js
@@ -442,6 +442,7 @@ var HomeView = Backbone.View.extend({
 
             var step = 360 / this.collection.length;
             var toctocAudio = document.getElementById('toctoc');
+            var selectedDay;
 
             $(document).on('mousemove', function(e) {
                 if (dragging) {
@@ -450,9 +451,12 @@ var HomeView = Backbone.View.extend({
                     $target.css('transform', 'rotate(' + degree + 'deg)');
                     $.each($('.day'), function(key, value) {
                         if (self.checkForOverlap($target.find('.select'), $(value))) {
-                            toctocAudio.play();
-                            $(value).removeClass('almostFocus').addClass('focus');
-                            $('.day:nth-child(' + (key - 2) + '), .day:nth-child(' + (key) + ')').removeClass('focus').addClass('almostFocus');
+                            if (selectedDay !== value) {
+                                selectedDay = value;
+                                toctocAudio.play();
+                                $(value).removeClass('almostFocus').addClass('focus');
+                                $('.day:nth-child(' + (key - 2) + '), .day:nth-child(' + (key) + ')').removeClass('focus').addClass('almostFocus');
+                            }
                         }
                     });
                 }
